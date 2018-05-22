22971544bf48497696d33319b28f62c8d948eb75
Jesse <boycht@gmail.com>
2018-05-22 23:59:12 +0800

    Add udp tunnel route which require administrator permission, so can change default route to redirect all traffic to minivtun. eg. 10.7.0.1 is minivtun server gateway address   route change 0.0.0.0 mask 0.0.0.0 10.7.0.1

--- tun.py
+++ tun.py
@@ -20,13 +20,18 @@ import getopt
 import random
 import signal
 import socket
+import subprocess
 import time
 import hashlib
-from pprint import pprint
+import logging
+import pprint
 import _winreg as reg
 import win32file
-import pywintypes, win32event
+import wmi
+import pywintypes
+import win32event
 import ipaddress
+import threading
 
 
 import dpkt
@@ -39,9 +44,12 @@ from dpkt.icmp import ICMP
 from struct import pack
 from struct import unpack
 
+FORMAT = '%(asctime)-15s %(levelname)-s: %(message)s'
+logger = logging.getLogger(__name__)
+
 
 unused_input_buffer = 'unused' # workaround for NIDS 6 dirver
-unused_output_buffer = win32file.AllocateReadBuffer(4) # workaround for NIDS 6 dirver
+unused_output_buffer = win32file.AllocateReadBuffer(64) # workaround for NIDS 6 dirver
 
 completion_port = None
 handle = None
@@ -60,23 +68,24 @@ crypto_type = "aes_128_cbc"
 now = time.time()
 last_send = now
 running = True
+delete_udp_tunnel_route = None
 
-import threading
 class TimerThread(threading.Thread):
     def __init__(self, interval = None):
         self.handle = win32event.CreateWaitableTimer(None, 0, None)
         if interval:
             self.set_timer(interval)
-        
+
         # initialize parent
         threading.Thread.__init__(self)
-        
+
     def run(self):
         global completion_port, running
+        logger.info('timer thread started')
         while running:
             win32file.PostQueuedCompletionStatus(completion_port, 0, 0, None)
             win32event.WaitForSingleObject(self.handle, win32event.INFINITE)
-            
+
     def set_timer(self, interval):
         global keepalive_interval
         win32event.SetWaitableTimer(self.handle, -10000000 * interval, 1000 * interval, None, None, 0)
@@ -88,7 +97,7 @@ cipher_pairs = {
 	"des": "des_cbc",
 	"desx": "desx_cbc",
 	"rc4": "rc4",
-};
+}
 
 
 AES_IVEC_INITVAL = ''.join(map(chr, (0xab, 0xcd, 0xef, 0x12, 0x34, 0x56, 0x78, 0x90,
@@ -124,7 +133,7 @@ def decrypt(key, data):
         v = v + cipher.final()
         del cipher
     except Exception as e:
-        print e
+        logger.error(e)
     return v
 
 def local_to_netmsg(data):
@@ -132,7 +141,7 @@ def local_to_netmsg(data):
         return encrypt(password_md5, data)
     else:
         return data
-    
+
 def netmsg_to_local(data):
     if password:
         return decrypt(password_md5, data)
@@ -160,7 +169,7 @@ def get_device_guid():
 METHOD_BUFFERED = 0
 
 def CTL_CODE(device_type, function, method, access):
-    return (device_type << 16) | (access << 14) | (function << 2) | method;
+    return (device_type << 16) | (access << 14) | (function << 2) | method
 
 def TAP_WIN_CONTROL_CODE(request, method):
     return CTL_CODE(34, request, method, 0)
@@ -174,7 +183,7 @@ TAP_WIN_IOCTL_SET_MEDIA_STATUS = TAP_WIN_CONTROL_CODE (6, METHOD_BUFFERED)
 TAP_WIN_IOCTL_CONFIG_DHCP_MASQ = TAP_WIN_CONTROL_CODE (7, METHOD_BUFFERED)
 TAP_WIN_IOCTL_GET_LOG_LINE = TAP_WIN_CONTROL_CODE (8, METHOD_BUFFERED)
 TAP_WIN_IOCTL_CONFIG_DHCP_SET_OPT = TAP_WIN_CONTROL_CODE (9, METHOD_BUFFERED)
-# obsoletes TAP_WIN_IOCTL_CONFIG_POINT_TO_POINT 
+# obsoletes TAP_WIN_IOCTL_CONFIG_POINT_TO_POINT
 TAP_WIN_IOCTL_CONFIG_TUN = TAP_WIN_CONTROL_CODE (10, METHOD_BUFFERED)
 
 
@@ -195,7 +204,7 @@ ETH_P_IPV6 = 0x86DD
 MINIVTUN_MSG_KEEPALIVE = 0
 MINIVTUN_MSG_IPDATA = 1
 MINIVTUN_MSG_DISCONNECT = 2
-    
+
 class Msg(dpkt.Packet):
     __hdr__ = (
         ('opcode', 'B', MINIVTUN_MSG_IPDATA),
@@ -203,19 +212,19 @@ class Msg(dpkt.Packet):
         ('passwd_md5sum', '16s', '\x00' * 16)
         )
 
-    
+
 class IPData(dpkt.Packet):
     __hdr__ = (
         ('proto', 'H', ETH_P_IP),
         ('ip_dlen', 'H', 0)
         )
-    
+
 class KeepAlive(dpkt.Packet):
     __hdr__ = (
         ('loc_tun_in', '4s', '\x00' * 4),
         ('loc_tun_in6', '16s', '\x00' * 16)
         )
-    
+
 def pack_keepalive(ip):
     ka = KeepAlive(loc_tun_in = ip)
     msg = Msg(data = ka, opcode = MINIVTUN_MSG_KEEPALIVE)
@@ -230,22 +239,22 @@ def pack_header(data):
     msg = Msg(data = ipdata)
     if password:
         msg.passwd_md5sum = password_md5
-    
+
     s = str(msg)
-    #print dpkt.dpkt.hexdump(s)
+    #logger.debug(dpkt.dpkt.hexdump(s))
     return s
 
 def unpack_header(s):
-    #print dpkt.dpkt.hexdump(s)
+    #logger.debug(dpkt.dpkt.hexdump(s))
     msg = Msg()
     msg.unpack(s)
-    
+
     if msg.opcode == MINIVTUN_MSG_KEEPALIVE:
         return
-    
+
     ipdata = IPData()
     ipdata.unpack(msg.data)
-    
+
     # data ends with AES padding
     if ipdata.ip_dlen > len(ipdata.data):
         return
@@ -254,69 +263,69 @@ def unpack_header(s):
 
 
 def keepalive():
-    # only send when no client -> server traffice
+    # only send when no client -> server traffic
     global sock, adapter_ip, now, last_send
     s = pack_keepalive(adapter_ip.packed)
     sock.sendall(local_to_netmsg(s))
     last_send = now
-    
-    
+
+
 class NetworkRecv():
     def __init__(self):
         self.overlapped_tx         = pywintypes.OVERLAPPED()
         #self.overlapped_tx.hEvent  = win32event.CreateEvent(None, 0, 0, None)
-        
+
         self.overlapped_rx         = pywintypes.OVERLAPPED()
         #self.overlapped_rx.hEvent  = win32event.CreateEvent(None, 0, 0, None)
-        
+
         generator = self.run()
         self.overlapped_tx.object = generator
         self.overlapped_rx.object = generator
         generator.next()
-        
-        
+
+
     def run(self):
         global sock, handle, mtu_size, verbose
         buf = win32file.AllocateReadBuffer(2000)
         while True:
             rc, bytes_recvd = win32file.WSARecv(sock.fileno(), buf, self.overlapped_rx)
             assert rc == 0 or rc == win32file.WSA_IO_PENDING
-            
+
             bytes_recvd = yield
-                
+
             p = buf[:bytes_recvd]
-                    
+
             p = netmsg_to_local(p)
             p = unpack_header(p)
             if p:
                 if verbose:
-                    print 'tunnel send: '
-                    
+                    logger.debug('tunnel send: ')
+
                     if (ord(p[0])&0xf0) == 0x40:
-                        pprint(IP(p))
+                        logger.debug(pprint.pformat(IP(p)))
                     elif (ord(p[0])&0xf0)==0x60:
-                        pprint(IP6(p))
+                        logger.debug(pprint.pformat(IP6(p)))
                     else:
-                        print 'Unknown layer 3 protocol'
-                    
+                        logger.warning('Unknown layer 3 protocol')
+
                 win32file.WriteFile(handle, p, self.overlapped_tx)
                 yield
-                
-                #print 'tunnel send complete'
-      
+
+                #logger.debug('tunnel send complete')
+
 class TunnelRecv():
     def __init__(self):
         self.overlapped_tx         = pywintypes.OVERLAPPED()
         #self.overlapped_tx.hEvent  = win32event.CreateEvent(None, 0, 0, None)
-        
+
         self.overlapped_rx         = pywintypes.OVERLAPPED()
         #self.overlapped_rx.hEvent  = win32event.CreateEvent(None, 0, 0, None)
-        
+
         generator = self.run()
         self.overlapped_tx.object = generator
         self.overlapped_rx.object = generator
         generator.next()
-        
+
     def run(self):
         global sock, handle, mtu_size, verbose, now, last_send
         buf = win32file.AllocateReadBuffer(mtu_size)
@@ -331,27 +340,27 @@ class TunnelRecv():
 
             # overlapped mode, return a PyOVERLAPPEDReadBuffer instead of str
             p = buf[:bytes_read]
-            
+
             if verbose:
-                print 'tunnel recv: '
+                logger.debug('tunnel recv: ')
                 #pprint(Ethernet(p))
                 if (ord(p[0])&0xf0) == 0x40:
-                    pprint(IP(p))
+                    logger.debug(pprint.pformat(IP(p)))
                 elif (ord(p[0])&0xf0)==0x60:
-                    pprint(IP6(p))
+                    logger.debug(pprint.pformat(IP6(p)))
                 else:
-                    print 'Unknown layer 3 protocol'
+                    logger.warning('Unknown layer 3 protocol')
                     continue # not support
-            
+
             #sock.sendall(local_to_netmsg(pack_header(p)))
             rc, bytes_sent = win32file.WSASend(sock.fileno(), local_to_netmsg(pack_header(p)), self.overlapped_tx)
-            
+
             # even send not pending, still generate a IOCP queued message
             bytes_sent = yield
             assert rc == 0 or rc == win32file.WSA_IO_PENDING
-            
+
             last_send = now
-                
+
 def usage():
     print """
     Mini virtual tunneller in non-standard protocol.
@@ -369,17 +378,36 @@ def usage():
       %s
     """ % (sys.argv[0], keepalive_interval,
            crypto_type, ', '.join(cipher_pairs.keys()))
-    
+
 def gen_dhcp_server(interface):
     for i in interface.network.hosts():
         if i != interface.ip:
             return i
-        
+
+def get_default_gateway(ip):
+    c = wmi.WMI()
+
+    for i in c.Win32_NetworkAdapterConfiguration(["IPAddress", "DefaultIPGateway", "IPEnabled"], IPEnabled=1):
+        if ip in i.IPAddress:
+            return i.DefaultIPGateway
+
+    return None
+
+def add_udp_tunnel_route(remote, local):
+    global delete_udp_tunnel_route
+    gateway = get_default_gateway(local)
+    logger.info('Found gateway %s for address %s', gateway, local)
+    if gateway:
+        cmd = 'route add {} mask 255.255.255.255 {}'.format(remote, gateway[0])
+        delete_udp_tunnel_route = 'route delete {} mask 255.255.255.255 {}'.format(remote, gateway[0])
+        logger.info(cmd)
+        subprocess.check_call(cmd)
+
 def sig_handler(signum, frame):
     global running
-    print 'Signal handler called with signal', signum
+    logger.info('Signal handler called with signal %d', signum)
     running = False
-    
+
 if __name__ == '__main__':
     # /usr/sbin/minivtun -r vpn.abc.com:1414 -a 10.7.0.33/24 -e Hello -d
     optlist, args = getopt.getopt(sys.argv[1:], 'r:a:k:t:e:dh',
@@ -411,6 +439,7 @@ if __name__ == '__main__':
         else:
             assert False, "Unhandled option %s" % (o, )
 
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO, format=FORMAT)
     if not server_ip:
         sys.exit('peer address required')
 
@@ -418,85 +447,98 @@ if __name__ == '__main__':
         dhcp_server = gen_dhcp_server(adapter_ip)
     else:
         sys.exit('tunnel IP address required')
-    
-    guid = get_device_guid()
-    # must be OVERLAPPED, otherwise write action will be blocked by read
-    handle = win32file.CreateFile(r'\\.\Global\%s.tap' % guid,
-                                  win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-                                  win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE,
-                                  None, win32file.OPEN_EXISTING,
-                                  win32file.FILE_ATTRIBUTE_SYSTEM | win32file.FILE_FLAG_OVERLAPPED,
-                                  None)
-    
-    mtu_size = unpack('I', win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_GET_MTU,
-                                  unused_input_buffer, 4, None))[0];
-    
-    win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_SET_MEDIA_STATUS, '\x01\x00\x00\x00', unused_output_buffer)
-    if False:
-        #adapter_ip = point_to_point[0]
-        # adapter ip, remote ip
-        win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_POINT_TO_POINT,
-                                  point_to_point[0].packed + point_to_point[1].packed, unused_output_buffer);
-    else:
-        # ip, network, mask
-        # 10.3.0.8 10.3.0.0 255.255.255.0
-        win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_TUN,
-                                  adapter_ip.packed + adapter_ip.network.network_address.packed + adapter_ip.netmask.packed,
-                                  unused_output_buffer)
-        # adpter ip, adpter mask, dhcp server ip, lease time in seconds (host order)
-        # 10.3.0.8 255.255.255.0 10.3.0.1 1200s
-        win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_DHCP_MASQ,
-                                  adapter_ip.packed + adapter_ip.netmask.packed + dhcp_server.packed +'\x10\x0e\x00\x00',
-                                  unused_output_buffer)
-    
-    addreses = socket.getaddrinfo(server_ip, server_port, socket.AF_INET, 0, socket.SOL_UDP)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    for addr in addreses:
-        try:
-            sock.connect(addr[4])
-            break
-        except socket.error as e:
-            print 'connect error: ', e
-    
-    print 'connect OK'
-        
-    signal.signal(signal.SIGINT, sig_handler)
-    
-    completion_port = win32file.CreateIoCompletionPort(win32file.INVALID_HANDLE_VALUE, None, 0, 0)
-    print win32file.CreateIoCompletionPort(handle, completion_port, 111, 0)
-    print win32file.CreateIoCompletionPort(sock.fileno(), completion_port, 222, 0)
-    
-    tun_recv = TunnelRecv()
-    net_recv = NetworkRecv()
-    
-    timer = TimerThread(1) # per second
-    timer.start()
-    
-    while running:
-        timeout = last_send + keepalive_interval - now
-        rc, numberOfBytesTransferred, completionKey, overlapped = win32file.GetQueuedCompletionStatus(completion_port, int(1000 * timeout))
-        if rc:
-            if rc == win32event.WAIT_TIMEOUT:
-                pass
-            else:
-                print "error %d" % rc
-                break
+
+    try:
+        guid = get_device_guid()
+        # must be OVERLAPPED, otherwise write action will be blocked by read
+        handle = win32file.CreateFile(r'\\.\Global\%s.tap' % guid,
+                                      win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                                      win32file.FILE_SHARE_READ | win32file.FILE_SHARE_WRITE,
+                                      None, win32file.OPEN_EXISTING,
+                                      win32file.FILE_ATTRIBUTE_SYSTEM | win32file.FILE_FLAG_OVERLAPPED,
+                                      None)
+
+        mtu_size = unpack('I', win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_GET_MTU,
+                                                         unused_input_buffer, 4, None))[0]
+
+        win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_SET_MEDIA_STATUS, '\x01\x00\x00\x00', unused_output_buffer)
+        if False:
+            #adapter_ip = point_to_point[0]
+            # adapter ip, remote ip
+            win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_POINT_TO_POINT,
+                                      point_to_point[0].packed + point_to_point[1].packed, unused_output_buffer)
         else:
-            if overlapped and overlapped.object:
-                overlapped.object.send(numberOfBytesTransferred)
+            # ip, network, mask
+            # 10.3.0.8 10.3.0.0 255.255.255.0
+            win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_TUN,
+                                      adapter_ip.packed + adapter_ip.network.network_address.packed + adapter_ip.netmask.packed,
+                                      unused_output_buffer)
+            # adpter ip, adpter mask, dhcp server ip, lease time in seconds (host order)
+            # 10.3.0.8 255.255.255.0 10.3.0.1 1200s
+            win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_CONFIG_DHCP_MASQ,
+                                      adapter_ip.packed + adapter_ip.netmask.packed + dhcp_server.packed +'\x10\x0e\x00\x00',
+                                      unused_output_buffer)
+
+        addreses = socket.getaddrinfo(server_ip, server_port, socket.AF_INET, 0, socket.SOL_UDP)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for addr in addreses:
+            try:
+                sock.connect(addr[4])
+                if addr[0] == socket.AF_INET:
+                    # add route for server
+                    add_udp_tunnel_route(addr[4][0], sock.getsockname()[0])
+                break
+            except socket.error as e:
+                logger.error('connect error: %s', e)
+
+        logger.info('connect OK')
+
+        signal.signal(signal.SIGINT, sig_handler)
+
+        completion_port = win32file.CreateIoCompletionPort(win32file.INVALID_HANDLE_VALUE, None, 0, 0)
+        win32file.CreateIoCompletionPort(handle, completion_port, 111, 0)
+        win32file.CreateIoCompletionPort(sock.fileno(), completion_port, 222, 0)
+
+        tun_recv = TunnelRecv()
+        net_recv = NetworkRecv()
+
+        timer = TimerThread(1) # per second
+        timer.start()
+
+        while running:
+            timeout = last_send + keepalive_interval - now
+            rc, numberOfBytesTransferred, completionKey, overlapped = win32file.GetQueuedCompletionStatus(completion_port, int(1000 * timeout))
+            if rc:
+                if rc == win32event.WAIT_TIMEOUT:
+                    pass
+                else:
+                    logger.error("error %d", rc)
+                    break
             else:
-                # timer
-                now = time.time()
-        
-        if last_send + keepalive_interval <= now:
-            keepalive()
+                if overlapped and overlapped.object:
+                    overlapped.object.send(numberOfBytesTransferred)
+                else:
+                    # timer
+                    now = time.time()
 
-    
-    win32file.CloseHandle(completion_port)
-    sock.close()
-    win32file.CloseHandle(handle)
-    
+            if last_send + keepalive_interval <= now:
+                keepalive()
 
+    finally:
+        if delete_udp_tunnel_route:
+            logger.info(delete_udp_tunnel_route)
+            subprocess.call(delete_udp_tunnel_route)
+
+        if completion_port:
+            logger.info("close completion port")
+            win32file.CloseHandle(completion_port)
+        if sock:
+            logger.info("close udp socket")
+            sock.close()
+        if handle:
+            win32file.DeviceIoControl(handle, TAP_WIN_IOCTL_SET_MEDIA_STATUS, '\x00\x00\x00\x00', unused_output_buffer)
+            logger.info("close tap device")
+            win32file.CloseHandle(handle)
 
 
 
